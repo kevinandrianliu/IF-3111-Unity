c280fd6b7e67bc3fdba958ae5ee3e499a5bf846f
Kevin Andrian Liwinata <kevinandrianliu@gmail.com>
2019-03-17 02:11:16 +0700

    Add Main Menu, Quit, and Options

---
+++ Assets/Scripts/PlayerPersistence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPersistence : MonoBehaviour
+{
+    public static void SaveData(Settings settings){
+		PlayerPrefs.SetInt("fullScreen", settings.playerData.fullScreen?1:0);
+		PlayerPrefs.SetInt("resolutionIndex", settings.playerData.resolutionIndex);
+		PlayerPrefs.SetFloat("musicVolume", settings.playerData.musicVolume);
+		PlayerPrefs.SetString("colorBall", settings.playerData.colorBall);
+		PlayerPrefs.SetInt("backgroundIndex", settings.playerData.backgroundIndex);		
+	}
+
+    public static PlayerData LoadData(){
+		float volume = PlayerPrefs.GetFloat("volume");
+		int resolution_index = PlayerPrefs.GetInt("resolutionIndex");
+		string color_ball = PlayerPrefs.GetString("colorBall");
+		bool full_screen = PlayerPrefs.GetInt("fullScreen")==1?true:false;
+		int background_index = PlayerPrefs.GetInt("backgroundIndex");
+		PlayerData playerData = new PlayerData(){
+			musicVolume = volume,
+			resolutionIndex = resolution_index,
+			colorBall = color_ball,
+			fullScreen = full_screen,
+			backgroundIndex = background_index,
+		};
+		return playerData;
+	}
+}
